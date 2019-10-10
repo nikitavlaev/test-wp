a140056accb058ec2f1004710a4e5e6c3daccd4b
Nikita <nikitavlaev00@gmail.com>
2019-10-10 18:23:51 +0300

    fixed hashes in names

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output:{
     path: path.resolve(__dirname, 'dist'),     // путь к каталогу выходных файлов - папка public
     publicPath: '/',
-    filename: '[name].[hash].js'
+    filename: '[name].js'
   },
   devServer: {
     historyApiFallback: true,
